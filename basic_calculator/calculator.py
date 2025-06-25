a4ca8edea39d552c102ee367cf470c3825aaeae3
Robert <r.niyonkuru@alustudent.com>
2025-06-25 23:31:32 +0200

    Grp4 Initial Commit

--- basic_calculator/calculator.py
+++ basic_calculator/calculator.py
@@ -1,41 +1,64 @@
 #Define a function that takes three arguments two numbers and an operator(+,-,*,/)
-    # define calculator
+# define calculator
 def main ():
     calculator()
-
+    
 def calculator():
+    
     print("")
-    num1 = int(input("Enter the first number: "))
-    print("")
-    operator = input("Choose operator (+,-.*,/): ")
-    print("")
-    num2 = int(input("Enter the second number: "))
-    print("")
+# Checking if the user has entered only numbers for the first number    
+    while True:
+        input1 = input("Enter the first number: ")
+        if input1.isdigit():
+            num1 = int(input1)
+            break
+        else:
+            print("Invalid input!")
+            print("Please, enter the number.")
+            print("")
+# Checking if the user has entered the correct operator    
+    while True:
+        input3 = input("Choose operator (+,-.*,/): ")
+        if input3 not in ["+","-","*","/"]:
+            print("Wrong operator")
+            print("Choose between (+,-,*,/) and try again!")
+            print("")
+        else:
+            operator = input3 
+            break
+        
+# Checking if the user has entered only numbers for the second number      
+    while True:
+        input2 = input("Enter the second number: ")
+        if input2.isdigit():
+            num2 = int(input2)
+            break
+        else:
+            print("Invalid input!")
+            print("Please, enter the number.")
+            print("")
 
+# perform calculation based on the operator
     if operator == '+':
         result = num1 + num2
-        print(f"{num1} {operator} {num2} = {result}")
         print("")
     
     elif operator == '-':
         result = num1 - num2
-        print(f"{num1} {operator} {num2} = {result}")
         print("")
         
     elif operator == '*':
         result = num1 * num2
-        print(f"{num1} {operator} {num2} = {result}")
         print("")
     
     elif operator == '/':
-        result = num1 / num2
-        print(f"{num1} {operator} {num2} = {result}")
+        result = round(num1 / num2, 2)
         print("")
-    
-    else:
-        print ("Wrong operator")
-        print("")
-        print ("Choose between (+,-,*,/)")
-    
+        
+# Showing the Final Result
+    print(f"{num1} {operator} {num2} = {result}")
+    print("")
+
+# Calling the main function
 if __name__ == '__main__':
     main()
